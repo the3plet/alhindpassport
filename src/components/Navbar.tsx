4c77167052707d63e93eeb12657ec6fdabfac327
Srj <soorajp2002@gmail.com>
2025-03-12 15:14:50 +0530

    navbar fixed position and credential added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
     setVerified(localStorage.getItem("verified") || "");
   }, []);
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-white shadow-md fixed z-50 w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
